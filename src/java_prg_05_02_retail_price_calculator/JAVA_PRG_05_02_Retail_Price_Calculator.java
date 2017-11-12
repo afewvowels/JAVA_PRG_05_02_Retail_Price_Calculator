381f3e2a09c64761741ea811c4dee9d0997ee342
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-11 19:15:42 -0500

    Tested and working.

--- src/java_prg_05_02_retail_price_calculator/JAVA_PRG_05_02_Retail_Price_Calculator.java
+++ src/java_prg_05_02_retail_price_calculator/JAVA_PRG_05_02_Retail_Price_Calculator.java
@@ -5,6 +5,7 @@
  */
 package java_prg_05_02_retail_price_calculator;
 
+import java.util.Scanner;
 /**
  *
  * @author bluebackdev
@@ -14,8 +15,47 @@ public class JAVA_PRG_05_02_Retail_Price_Calculator {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
+        calculateRetailPrice(getItemPrice(), getItemMarkup());
     }
     
+    public static double getItemPrice()
+    {
+        double dblPrice;
+        
+        Scanner scrKeyboard = new Scanner(System.in);
+        
+        System.out.print("Please enter the item's cost: $");
+        
+        dblPrice = scrKeyboard.nextDouble();
+        
+        return dblPrice;
+    }
+    
+    public static double getItemMarkup()
+    {
+        double dblMarkup;
+        
+        Scanner scrKeyboard = new Scanner(System.in);
+        
+        System.out.print("Please enter the item's markup by percent: ");
+        
+        dblMarkup = scrKeyboard.nextDouble();
+        
+        dblMarkup = dblMarkup / 100.0;
+        
+        return dblMarkup;
+    }
+    
+    public static void calculateRetailPrice(double dblPrice, double dblMarkup)
+    {
+        double dblRetail;
+        
+        dblRetail = dblPrice + (dblPrice * dblMarkup);
+        
+        System.out.println("With a wholesale cost of $" + dblPrice + " and a" +
+                " markup of " + (dblMarkup * 100) + "%, the retail price is: $" +
+                dblRetail);
+    }
 }
